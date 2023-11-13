c08c06e0a205b68c250753caea7f1d20a52b6fc9
Oliver <oliver.liljeblad@gmail.com>
2023-11-13 10:20:47 -0500

    Changed manu from text to buttons for function.

--- main.py
+++ main.py
@@ -1,57 +1,78 @@
 import pygame
-from random import randint
+import sys
+from char import Char
 
+# Initialize Pygame
 pygame.init()
 
+# Constants
 WIDTH = 800
 HEIGHT = 600
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
+FONT_SIZE = 36
+BUTTON_WIDTH = 200
+BUTTON_HEIGHT = 50
 
-class Menu:
-    def __init__(self, screen, items, font, font_size, item_spacing=50):
+# Button class
+class Button:
+    def __init__(self, screen, text, position, font_size):
         self.screen = screen
-        self.items = items
+        self.text = text
+        self.position = position
         self.font = pygame.font.Font(None, font_size)
-        self.selected_item = 0
-        self.item_spacing = item_spacing
+        self.width = BUTTON_WIDTH
+        self.height = BUTTON_HEIGHT
+
+    def draw(self, selected):
+        rect_color = WHITE if not selected else (200, 200, 200)
+        pygame.draw.rect(self.screen, rect_color, (*self.position, self.width, self.height))
+        text = self.font.render(self.text, True, BLACK)
+        text_rect = text.get_rect(center=(self.position[0] + self.width // 2, self.position[1] + self.height // 2))
+        self.screen.blit(text, text_rect)
+
+# Menu class
+class Menu:
+    def __init__(self, screen, buttons, font_size):
+        self.screen = screen
+        self.buttons = buttons
+        self.selected_button = 0
 
     def draw(self):
-        for i, item in enumerate(self.items):
-            text = self.font.render(item, True, WHITE if i == self.selected_item else BLACK)
-            text_rect = text.get_rect(center=(self.screen.get_width() // 2, self.screen.get_height() // 2 + i * self.item_spacing))
-            self.screen.blit(text, text_rect)
+        for i, button in enumerate(self.buttons):
+            button.draw(i == self.selected_button)
 
     def select(self):
-        # Handle the action for the currently selected menu item
-        if self.selected_item == 0:
+        # Handle the action for the currently selected button
+        if self.selected_button == 0:
             print("Start Game")
-        elif self.selected_item == 1:
-            print("Options")
-        elif self.selected_item == 2:
+            # Add "Start Game" logic here
+        elif self.selected_button == 1:
+            print("How to Play")
+            # Add "How to Play" logic here
+        elif self.selected_button == 2:
             pygame.quit()
             sys.exit()
 
     def navigate(self, direction):
-        # Update the selected menu item based on the direction (1 for down, -1 for up)
-        self.selected_item = (self.selected_item + direction) % len(self.items)
+        # Update the selected button based on the direction (1 for down, -1 for up)
+        self.selected_button = (self.selected_button + direction) % len(self.buttons)
 
-
+# Main function
 def main():
-    pygame.init()
     # Display configuration
-    screen = pygame.display.set_mode( (WIDTH,HEIGHT) )
-    
-    pygame.display.set_caption("Final Project")
-    background = pygame.Surface(screen.get_size())
-    background = background.convert()
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
+    pygame.display.set_caption("Button Menu")
 
-    # Draw the background on the screen before the loop to start with a fresh view
-    screen.blit(background, (0,0))
+    # Create buttons for the menu
+    buttons = [
+        Button(screen, "Start Game", (WIDTH // 2 - BUTTON_WIDTH // 2, HEIGHT // 2 - BUTTON_HEIGHT-30), FONT_SIZE),
+        Button(screen, "How to Play", (WIDTH // 2 - BUTTON_WIDTH // 2, HEIGHT // 2 - 15), FONT_SIZE),
+        Button(screen, "Quit", (WIDTH // 2 - BUTTON_WIDTH // 2, HEIGHT // 2 + BUTTON_HEIGHT), FONT_SIZE),
+    ]
 
-    items = ["Start Game", "Options", "Exit"]
-    font_size = 36
-    menu = Menu(screen, items, None, font_size)
+    # Create a menu instance
+    menu = Menu(screen, buttons, FONT_SIZE)
 
     clock = pygame.time.Clock()
     running = True
@@ -63,19 +84,16 @@ def main():
             if event.type == pygame.QUIT:
                 running = False
             if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_ESCAPE or event.key == pygame.K_q:
-                    running = False
                 if event.key == pygame.K_UP:
                     menu.navigate(-1)
-                if event.key == pygame.K_DOWN:
+                elif event.key == pygame.K_DOWN:
                     menu.navigate(1)
-                if event.key == pygame.K_RETURN:
+                elif event.key == pygame.K_RETURN:
                     menu.select()
 
         screen.fill(BLACK)
         menu.draw()
         pygame.display.flip()
-        clock.tick(60)
 
     pygame.quit()
     sys.exit()
